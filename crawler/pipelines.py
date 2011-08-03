51c3513daf73c9a07f649daf919ec4d41611ca9e
Vladan Popovic <vladanovic@gmail.com>
2011-08-03 18:33:56 +0200

    Рефактор на функцијата за внесување во couchdb

--- crawler/pipelines.py
+++ crawler/pipelines.py
@@ -1,24 +1,31 @@
-#!/bin/python -*- coding=utf-8 -*-
+#!/usr/bin/python -*- coding=utf-8 -*-
 
 import couchdb
 import hashlib
-from . import settings
+import settings
 
 class CrawlerPipeline(object):
     """Запишување на податоците во CouchDB"""
     def __init__(self):
         self.db = couchdb.Database(settings.DATA_SETTINGS[settings.ENV]['couchdb_server'])
 
-    def process_item(self,  domain,  item):
+    def process_item(self, item, spider):
+        if item is None:
+            return item
         # Креирање на python dictionary од внесените податоци (со мета податоците)
         d = dict(item)
         d['meta'] = dict(item.meta)
-        # Генерирање на уникатед ID од насловот и авторот
-        d_id = hashlib.md5("%s%s" % (item['name'], item.meta.get('author', 'mkwordnet'))).hexdigest()
-        if d_id in self.db and self.db[d_id]['text'].find(item['text']) != -1:
-            # ако постои документ од авторот со ист наслов додади го новиот текстот на постоечкиот
-            doc = self.db[d_id]
-            doc['text'] = "%s\n%s" % (doc['text'], d['text'])
-            d = doc
-        self.db[d_id] = d
+        # Генерирање на уникатед ID од URL-то
+        d_id = hashlib.md5(d['meta']['url']).hexdigest()
+
+        if d_id in self.db:
+            doc = self.db.get(d_id)
+            if len(doc['text']) > 0 and doc['text'].find(d['text']) != -1:
+                # ако постои документ од авторот со ист наслов додади го новиот текстот на постоечкиот
+                doc = self.db.get(d_id)
+                doc['text'] = "%s\n%s" % (doc['text'], d['text'])
+        else:
+            doc = d
+
+        self.db[d_id] = doc
         return item
