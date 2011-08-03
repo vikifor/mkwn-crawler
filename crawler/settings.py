456a6f9df93d2719adf857d757a9722fb90c33fb
Vladan Popovic <vladanovic@gmail.com>
2011-08-03 18:32:48 +0200

    Додадени подесувања за пауза меѓу request-и

--- crawler/settings.py
+++ crawler/settings.py
@@ -1,4 +1,4 @@
-#!/bin/python -*- coding=utf-8 -*-
+#!/usr/bin/python -*- coding=utf-8 -*-
 # Scrapy settings for crawler project
 #
 # For simplicity, this file contains only the most important settings by
@@ -7,24 +7,33 @@
 #     http://doc.scrapy.org/topics/settings.html
 #
 
-BOT_NAME = 'MkWordnet scraper - Scrapy 0.10.4 (Македонска верзија на Princeton Wordnet)'
+BOT_NAME = 'crawler'
 BOT_VERSION = '0.1'
 
 SPIDER_MODULES = ['crawler.spiders']
 NEWSPIDER_MODULE = 'crawler.spiders'
 DEFAULT_ITEM_CLASS = 'crawler.items.TextDataItem'
-USER_AGENT = '%s/%s' % (BOT_NAME, BOT_VERSION)
+USER_AGENT = '%s/%s' % ('MkWordnet scraper - Scrapy 0.12 (Македонска верзија на Princeton Wordnet)', BOT_VERSION)
 
-import os
+HTTPCACHE_DIR = "cache"
+HTTPCACHE_EXPIRATION_SECS = 3200
+DOWNLOAD_DELAY = 0.25 # 250 ms of delay
+
+ITEM_PIPELINES = [
+    'crawler.pipelines.CrawlerPipeline',
+]
+
+DOWNLOAD_DELAY = 4
 
 ENV = 'local'
 
 DATA_SETTINGS = {
     'local':{
         'couchdb_server':'http://localhost:5984/mkwordnet_db',
-        'db':os.path.join(os.path.dirname(__file__), '../../test.db'),
      },
     'remote':{
         'couchdb_server':'http://mkwordnet.hopto.org:5984/mkwordnet_db',
      }
 }
+# LOG_LEVEL='INFO'
+
